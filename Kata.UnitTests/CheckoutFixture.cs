d3ebb80da708eee44b939e5a31acc2119ad3f263
Dan Ormisher <dan@danielormisher.com>
2020-03-30 14:44:27 +0100

    Checkout can now scan multiple items

--- Kata.UnitTests/CheckoutFixture.cs
+++ Kata.UnitTests/CheckoutFixture.cs
@@ -5,6 +5,10 @@ namespace Kata.UnitTests
 {
     public class CheckoutFixture
     {
+        private readonly Item _apple = new Item("A99", 0.5m);
+        private readonly Item _biscuits = new Item("B15", 0.3m);
+        private readonly Item _cola = new Item("C40", 0.6m);
+        
         [Fact]
         public void Total_NoScannedItem_ShouldBeZero()
         {
@@ -15,16 +19,25 @@ namespace Kata.UnitTests
         [Fact]
         public void TotalShouldMatch_SingleItemUnitPrice_WhenSingleItem_IsScanned()
         {
-            // Arrange
-            const string sku = "A99";
-            const decimal unitPrice = 0.5m;
-            
             // Act
             var checkout = new Checkout();
-            checkout.Scan(new Item(sku, unitPrice));
+            checkout.Scan(_apple);
             
             // Assert
             checkout.Total().Should().Be(0.5m);
+        }
+        
+        [Fact]
+        public void TotalShouldMatch_SumOfMultipleUnitPrices_WhenManyItems_AreScanned()
+        {
+            // Act
+            var checkout = new Checkout();
+            checkout.Scan(_apple);
+            checkout.Scan(_biscuits);
+            checkout.Scan(_cola);
+            
+            // Assert
+            checkout.Total().Should().Be(1.4m);
         }
     }
 }
